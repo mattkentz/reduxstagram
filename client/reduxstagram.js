1191a94d893bcc9b91c3adcaf5f6918167d2e755
Matthias Kentzia <kentzia@gmail.com>
2016-10-19 20:09:37 +0200

    #Added Sentry error tracking

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -12,6 +12,15 @@ import App from './components/App';
 import Photogrid from './components/Photogrid';
 import Single from './components/Single';
 
+// Error Tracking using Sentry
+import Raven from 'raven-js';
+import { sentry_url } from './data/config';
+
+Raven.config(sentry_url, {
+    git_comment: 'git_commit_hash',
+    userLevel: 'loggedInMember'
+}).install();
+
 const router = (
     <Provider store={store}>
         <Router history={history}>
