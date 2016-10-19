69da47c25ac3cf11e5c0dd89f1a04c4cccaeb687
Matthias Kentzia <kentzia@gmail.com>
2016-10-19 20:12:58 +0200

    #Setup hot reloading for reducers

--- client/store.js
+++ client/store.js
@@ -16,4 +16,12 @@ const defaultState = {
 const store = createStore(rootReducer, defaultState);
 export const history = syncHistoryWithStore(browserHistory, store);
 
+// Hot reload reducers
+if (module.hot) {
+    module.hot.accept('./reducers/', () => {
+        const nextRootReducer = require('./reducers/index').default;
+        store.replaceReducer(nextRootReducer);
+    });
+}
+
 export default store;
