2e844b47b4f586f07bef9e7801a629c1f08c962d
Matthias Kentzia <kentzia@gmail.com>
2016-10-19 18:54:32 +0200

    #Added Photo and updated Photogrid and Posts reducer to increment likes

--- client/components/Photogrid.js
+++ client/components/Photogrid.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router';
 
+import Photo from './Photo';
+
 const Photogrid = React.createClass({
     render() {
         return (
             <div className="photo-grid">
-                Photogrid
+                {this.props.posts.map((post, index) => {
+                    return <Photo {...this.props} key={index} index={index} post={post}/>
+                })}
             </div>
         )
 
--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,7 +1,11 @@
 function posts(state = [], action) {
-    switch (action) {
+    switch (action.type) {
         case 'INCREMENT_LIKES': {
-
+            return [
+                ...state.slice(0, action.index),
+                {...state[action.index], likes: state[action.index].likes +1},
+                ...state.slice(action.index +1 )
+            ]
         }
 
         default: {
